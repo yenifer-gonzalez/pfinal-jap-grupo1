9671fb0544f357bfa811fffdaeeab54d9eb4f309
NRDEV1771 <nahueregue@gmail.com>
2025-08-18 14:50:04 -0300

    commit n2 - entrega_01 JSON FETCH

---
+++ js/products.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Relleno de data para hacer la pèticion Web
+    // URL fija para la categoría 101 de autos 
+
+  const url = PRODUCTS_URL + "101" + EXT_TYPE;
+
+  getJSONData(url).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      const productos = resultObj.data.products;
+      mostrarProductos(productos);
+    }
+  });
+});
+
+function mostrarProductos(productos) {
+  let html = "";
+
+  productos.forEach(producto => {
+    html += `
+      <div>
+        <h3>${producto.name}</h3>
+        <p>${producto.description}</p>
+        <p>Precio: ${producto.currency} ${producto.cost}</p>
+        <p>Vendidos: ${producto.soldCount}</p>
+        <img src="${producto.image}" alt="${producto.description}">
+      </div>
+    `;
+  });
+
+  document.getElementById("product-list-container").innerHTML = html;
+}
